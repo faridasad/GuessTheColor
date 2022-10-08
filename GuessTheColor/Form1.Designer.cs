41b3955c00d46c0c714814fd3406968286f3e896
faridasad <asadlifaridd@gmail.com>
2022-10-08 23:43:10 +0400

    Add project files.

---
+++ GuessTheColor/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace GuessTheColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorBox = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ColorBox
+            // 
+            this.ColorBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ColorBox.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ColorBox.Location = new System.Drawing.Point(89, 79);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(586, 168);
+            this.ColorBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GUESS THE COLOR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 64);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(454, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 64);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 402);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 64);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(454, 402);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 64);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "C)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(110, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "A)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(414, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "B)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(414, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 26);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "D)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 519);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ColorBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ColorBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GuessTheColor/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheColor
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> main_colors = new List<string>() { "Brown", "Black", "Pink", "Blue",
+            "BlueViolet", "Green", "Yellow", "Orange",
+            "Red", "White", "Gray", "Gold" };
+
+        List<string> random_colors = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            displayQuestion();
+        }
+
+        public void displayQuestion()
+        {
+            if (main_colors.Count < 4) return;
+
+            List<string> temp_colors = new List<string>(main_colors);
+
+            for (int i = 0; i < 4; i++)
+            {
+                int rndx = new Random().Next(temp_colors.Count);
+                random_colors.Add(temp_colors[rndx]);
+                temp_colors.RemoveAt(rndx);
+            }
+
+            int rnd = new Random().Next(4);
+            ColorBox.BackColor = Color.FromName(random_colors[rnd]);
+
+            button1.Text = random_colors[0];
+            button2.Text = random_colors[1];
+            button3.Text = random_colors[2];
+            button4.Text = random_colors[3];
+
+            random_colors.Clear();
+        }
+
+        public void gameOver(string msg)
+        {
+            main_colors = resetList();
+            MessageBox.Show(msg);
+        }
+
+        public List<string> resetList()
+        {
+            List<string> main_colors = new List<string>() { "Brown", "Black", "Pink", "Blue",
+            "BlueViolet", "Green", "Yellow", "Orange",
+            "Red", "White", "Gray", "Gold" };
+
+            return main_colors;
+        }
+
+        public void handleAnswer(string s)
+        {
+
+            if (ColorBox.BackColor.ToString() == $"Color [{s}]")
+            {
+                main_colors.Remove(s);
+                displayQuestion();
+
+                if (main_colors.Count < 4)
+                {
+                    gameOver("congrats");
+                    displayQuestion();
+                }
+            }
+            else
+            {
+                gameOver("I dərəcəli rəng koru");
+                displayQuestion();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            handleAnswer(s);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            handleAnswer(s);
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            handleAnswer(s);
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            handleAnswer(s);
+        }
+    }
+}
